2ae04d8d79422e9ae81f0cff5d75cecc02f4ffd8
Kaustubh Ranjan <kaustubhranjan5312@gmail.com>
2026-06-01 20:28:30 +0530

    First app

--- app/src/main/java/com/example/right_path/MainActivity.kt
+++ app/src/main/java/com/example/right_path/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             Right_PathTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Krish",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
